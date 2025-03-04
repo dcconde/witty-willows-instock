18d330939933343966ecbc4932a653e18120d214
Marison Federipe <marisonfdrp@gmail.com>
2025-03-04 08:02:30 -0800

    Fetch warehouse data in WarehousePage

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,12 +1,29 @@
 import "./WarehousePage.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 function WarehousePage() {
+  const [warehousesData, setWarehousesData] = useState([]);
+
+  const getWarehouses = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/api/warehouses`);
+      setWarehousesData(response.data);
+    } catch (err) {
+      console.error("Error getting warehouses", err);
+    }
+  };
+
+  useEffect(() => {
+    getWarehouses();
+  }, []);
+
   return (
     <>
-      <WarehouseList />
+      <WarehouseList warehousesData={warehousesData} />
     </>
   );
 }
