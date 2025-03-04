d6bba353e42774c06224f07cc79ff3ad1e222f07
Mankirat <mankiratcheema0047@gmail.com>
2025-03-04 13:35:22 -0600

    set up footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+function Footer(){
+    return(
+        <footer>
+            <div className="footer">
+                <p className="footer__legal">© InStock Inc. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
