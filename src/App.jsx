800823dc881a0fe4d08618103a75ba8588e6f419
Mankirat <mankiratcheema0047@gmail.com>
2025-03-05 06:35:01 -0600

    set up route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import DeleteInventory from "./components/DeleteInventory/DeleteInventory";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<WarehousePage />} />
         <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/deleteinventory" element={<DeleteInventory />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
