5ed9df1dd2bf9bca78c4d74b33adbc46ce675ede
Brightosey <Brightosey@users.noreply.github.com>
2025-03-05 01:50:29 -0500

    Refactored the Header component by replacing the div with a section, created separate JSX and SCSS files for it, updated the footer styles, and added global styles for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<WarehousePage />} />
         <Route path="/inventory" element={<InventoryPage />} />
