f8a8b8ce73cefb8600732ad8e6dd66c3b885b29a
Marison Federipe <marisonfdrp@gmail.com>
2025-03-07 16:26:12 -0800

    Fix Footer route

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,13 @@
-import "./Footer.scss";
+import "./footer.scss";
 
-function Footer(){
-    return(
-        <footer>
-            <div className="footer">
-                <p className="footer__legal">© InStock Inc. All Rights Reserved.</p>
-            </div>
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer>
+      <div className="footer">
+        <p className="footer__legal">© InStock Inc. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
