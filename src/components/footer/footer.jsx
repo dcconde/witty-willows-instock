8539fcfd75e1eac4122e46552b024e42d75aaf43
Marison Federipe <marisonfdrp@gmail.com>
2025-03-09 10:57:17 -0700

    Add return statement in footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,12 @@
 import "./footer.scss";
 
 function Footer() {
-  <footer className="footer">
-    <p className="footer__legal">© InStock Inc. All Rights Reserved.</p>
-  </footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__legal">© InStock Inc. All Rights Reserved.</p>
+    </footer>
+  );
 }
+
 
 export default Footer;
