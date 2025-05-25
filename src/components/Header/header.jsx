e6538e1d815db43069cdf2e20ad5ec63da3ea9cc
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-05-25 14:14:14 -0700

    update details

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,36 +1,36 @@
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import inStockLogo from "../../assets/logo/instock-logo.svg";
-import "./Header.scss";
+import "./header.scss";
 
 function Header() {
-  return (
-    <header className="header">
-      <Link to="/" className="header__logo-link">
-        <img src={inStockLogo} alt="instock logo" className="header__logo" />
-      </Link>
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <NavLink
-              to="/"
-              className="header__nav-link header__nav-link--warehouses"
-            >
-              Warehouses
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <NavLink
-              to="/inventory"
-              className="header__nav-link header__nav-link--inventory"
-            >
-              Inventory
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="header">
+			<Link to="/" className="header__logo-link">
+				<img src={inStockLogo} alt="instock logo" className="header__logo" />
+			</Link>
+			<nav className="header__nav">
+				<ul className="header__nav-list">
+					<li className="header__nav-item">
+						<NavLink
+							to="/"
+							className="header__nav-link header__nav-link--warehouses"
+						>
+							Warehouses
+						</NavLink>
+					</li>
+					<li className="header__nav-item">
+						<NavLink
+							to="/inventory"
+							className="header__nav-link header__nav-link--inventory"
+						>
+							Inventory
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
